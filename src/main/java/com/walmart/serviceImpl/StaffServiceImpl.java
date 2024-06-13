2f2171e3d7a19e3d0268e8e268c9066e1f26a5ee
ManeeshNadipelli <nadipelli.maneesh@gmail.com>
2024-06-13 11:16:44 -0400

    Added bussiness logic
    added business logic and staff controller

--- src/main/java/com/walmart/serviceImpl/StaffServiceImpl.java
+++ src/main/java/com/walmart/serviceImpl/StaffServiceImpl.java
@@ -1,5 +1,50 @@
 package com.walmart.serviceImpl;
 
+import com.walmart.model.Staff;
+
 public class StaffServiceImpl {
+	
+	public void assignDepartment(Staff staff)
+		    throws Exception
+		{
+		    String department = staff.getDepartment();
+		    
+		    if (department.equals("Groceries"))
+		    {
+		        staff.setDepartment("Groceries"); 
+		    }
+		    else if (department.equals("Clothing"))
+		    {
+		        staff.setDepartment("Clothing");  
+		    } 		
+		    else if (department.equals("Electronics"))
+		    {
+		        staff.setDepartment("Electronics");
+		    }
+		    else if (department.equals("Sports"))
+		    {
+		    	staff.setDepartment("Sports");
+		    }
+		    else if (department.equals("Home"))
+		    {
+		    	staff.setDepartment("Home");
+		    }
+		    else if (department.equals("Beauty"))
+		    {
+		    	staff.setDepartment("Beauty");
+		    }
+		    else if (department.equals("Pharmacy"))
+		    {
+		        staff.setDepartment("Pharmacy");
+		    }
+		    else if (department.equals("Auto"))
+		    {
+		        staff.setDepartment("Auto");
+		    }
+		    else
+		    {
+		        throw new Exception("Invalid Department");
+		    }
+		}
 
 }
