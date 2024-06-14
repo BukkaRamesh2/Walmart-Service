76d28a274860af56d9db1c2e5c641f9e8c87c799
sreedathsomisetty <sreedathsomisetty.ss@gmail.com>
2024-06-14 09:01:47 -0400

    made these changes

--- src/main/java/com/walmart/service/TransactionService.java
+++ src/main/java/com/walmart/service/TransactionService.java
@@ -1,5 +1,20 @@
 package com.walmart.service;
 
+import java.util.List;
+
+import com.walmart.model.Transaction;
+
 public interface TransactionService {
+
+	
+	Transaction saveTransaction(Transaction trans);
+	
+	Transaction getTransactionById(int transaction_id);
+	
+	List<Transaction> getAllTransactions();
+
+	Transaction updateTransaction(Transaction tarns, int transaction_id);
+
+	void deleteTransaction(int transaction_id);
 
 }
