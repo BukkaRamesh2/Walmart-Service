65b7d32fc294a83fa4383ba3fa7fb41c0ace95b8
Mythili Sistla <mythili.sistla@gmail.com>
2024-06-18 00:35:35 -0500

    Product Exception Added

---
+++ src/main/java/com/walmart/exception/ProductNotFoundException.java
@@ -0,0 +1,18 @@
+package com.walmart.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ProductNotFoundException {
+	
+	 @ExceptionHandler(ProductNotFoundException.class)
+	    public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException e) {
+	        ErrorResponse err = new ErrorResponse(e.getMessage());
+	        return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+	    }
+
+
+}
--- src/main/java/com/walmart/serviceImpl/ProductSericeImpl.java
+++ src/main/java/com/walmart/serviceImpl/ProductSericeImpl.java
@@ -1,8 +1,19 @@
 package com.walmart.serviceImpl;
 
-public class ProductSericeImpl {
+import java.util.List;
+import java.util.Optional;
 
-	public String getProductDescription(int productId) {		
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.walmart.model.Product;
+import com.walmart.repository.ProductRepository;
+
+public class ProductSericeImpl {
+	
+	@Autowired
+	ProductRepository productRepo;
+
+	public String getProductDescription(Long productId) {		
 		if(productId == 1) {
 			return "Product Description 1";
 		}else if(productId == 2) {
@@ -13,5 +24,41 @@ public class ProductSericeImpl {
 			return "Product Description for other products";
 		}		
 	};
+	
+	// method 1 with one parameter i.e. ID
+	public Product getProductById(Long productId) {
+		try {
+			
+			Optional<Product> product =  productRepo.findById(productId);
+            if (product.isPresent()) {
+                return product.get();
+            } else {
+                throw new ProductNotFoundException("Product with ID " + productId + " not found.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Finally block executed");
+        }
+        return null;
+    }
+
+		
+		// same method but different signature 
+	public Product getProductById(Long productId, String name) throws RuntimeException{
+		 Optional<Product> product =  productRepo.findById(productId);
+	        if(product.isPresent()){
+	            return product.get();
+	        }else {
+	            throw new RuntimeException();
+	        }
+	}
+		
+		
+
+	public List<Product> getAllProducts() {
+		return productRepo.findAll();
+	}
+	
 
 }
