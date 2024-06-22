780efbf3810e8d3b51b3f84308221bbb5ee4fd92
BukkaRamesh2 <rameshverudix@gmail.com>
2024-06-22 07:37:25 -0500

    Update ProductRepository.java

--- src/main/java/com/walmart/repository/ProductRepository.java
+++ src/main/java/com/walmart/repository/ProductRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.walmart.model.Product;
 
-public class ProductRepository extends JpaRepository<Product, Long>{
+public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
